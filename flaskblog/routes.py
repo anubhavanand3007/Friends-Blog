057284ebd92c24006e13f75732e75187ca03d60d
Anubhav Anand <anubhav-anand-30>
2021-09-11 14:50:18 +0530

    CREATED LOGIC AND QUERY FOR REGISTRATION OF USER

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template
-from flaskblog import app, conn
+from flask import render_template, flash, url_for, redirect
+from flaskblog import app, conn, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 
 posts = [
@@ -28,4 +28,27 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='Register')
+    fielderror = {}
+    form = RegistrationForm()
+    c = conn.cursor()
+    
+    c.execute(f"SELECT id FROM user WHERE email = '{form.email.data}'")
+    isEmail = c.fetchone()
+    c.execute(f"SELECT id FROM user WHERE username = '{form.username.data}'")
+    isUsername = c.fetchone()
+
+    if isEmail:
+        fielderror['email'] = 'This Email is already taken.'
+    if isUsername:
+        fielderror['username'] = 'This Usernanme is already taken.'
+
+    if form.validate_on_submit() and not isUsername and not isEmail:
+
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        c = conn.cursor()
+        c.execute(f"INSERT INTO user(username, email, password) VALUES ('{form.username.data}','{form.email.data}','{hashed_password}')")
+
+        conn.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form, fielderror = fielderror)
