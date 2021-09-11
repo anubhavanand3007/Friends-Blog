e023b2e7dd60e279493e761b1291ad6c25107f3b
Anubhav Anand <anubhav-anand-30>
2021-09-11 15:17:26 +0530

    imported session module

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, redirect, flash
+from flask import render_template, url_for, redirect, flash, session
 from flaskblog import app, conn, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 
