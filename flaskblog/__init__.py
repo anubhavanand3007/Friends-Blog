87f74cc79d7542246a3245ad4df9d78a38383ddd
Anubhav Anand <anubhav-anand-30>
2021-09-11 14:46:25 +0530

    improted hashing module
    created instance of flask-bcrypt
    configured ou app with a secret key

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 import sqlite3
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 conn = sqlite3.connect('site.db', check_same_thread=False)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
