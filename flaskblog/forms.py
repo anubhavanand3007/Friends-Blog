9e39d3c6df119caa8815e35b98d7fa3a785331d2
Anubhav Anand <anubhav-anand-30>
2021-09-14 10:35:10 +0530

    added a picture field in form.py/UpdateAccountForm class

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
@@ -20,4 +21,5 @@ class LoginForm(FlaskForm):
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=['jpg','png'])
     submit = SubmitField('Update')
