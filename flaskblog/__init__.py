befb93f00258d9d86280810448d302d3cc27446c
Anubhav Anand <88741077+anubhavanand3007@users.noreply.github.com>
2021-09-23 01:17:15 +0530

    Update __init__.py
    made public repo

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,7 +3,7 @@ import sqlite3
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = 'dummy'
 
 conn = sqlite3.connect('site.db', check_same_thread=False)
 bcrypt = Bcrypt(app)
